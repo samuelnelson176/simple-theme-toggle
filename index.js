8d738f5f4737809f6c5a6047a7bb0fb206360eb0
Nelson Samuel <106776244+samuelnelson176@users.noreply.github.com>
2022-10-02 17:09:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-const bg = [
-    "rgb(10, 0, 33)",
-    "rgb(255, 255, 255)"
-];
-
 function toggleBg() {
-    if (document.body.style.backgroundColor = bg[0]) {
-            document.body.style.backgroundColor = bg[1]
+    if (document.body.style.backgroundColor = "rgb(255, 255, 255)") {
+            document.body.style.backgroundColor = "rgb(10, 0, 33)"
     }
     else {
-        document.body.style.backgroundColor = bg[0]
+        document.body.style.backgroundColor = "rgb(10, 0, 33)"
     };
-    
-    
 };
 
-document.getElementById('btn').addEventListener("click", alert(bg[0]));
+document.getElementById('btn').addEventListener("click", () =>toggleBg());
